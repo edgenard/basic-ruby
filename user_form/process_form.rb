1aae8c78f717d8e0a8c862c46c3f1171a834f5bd
Emmanuel Genard <edgenard@gmail.com>
2020-07-04 18:42:42 -0400

    undo changes to process_form so that it passes CI

--- user_form/process_form.rb
+++ user_form/process_form.rb
@@ -3,9 +3,9 @@ require "aws-sdk-s3"
 module UserForm
   class ProcessForm
     def self.handler(event:, context:)
-      # body = URI.decode_www_form(event["body"]).to_h.to_json
+      body = URI.decode_www_form(event["body"]).to_h.to_json
       client = Aws::S3::Client.new(region: ENV["AWS_REGION"])
-      client.put_object(bucket: ENV["PROCESS_FORM_BUCKET"], key: context.aws_request_id, body: event["body"])
+      client.put_object(bucket: ENV["PROCESS_FORM_BUCKET"], key: context.aws_request_id, body: body)
       response = HtmlResponse.successfully_processed_form(context.aws_request_id)
       {
         statusCode: 201,
