135e922cf24e22c3060ea126919bdcde57905c21
Emmanuel Genard <edgenard@gmail.com>
2020-07-18 10:07:01 -0400

    Update set_stack_name to be more robust

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -1,14 +1,25 @@
 #!/bin/ruby
 
-def set_stack_name
-  app_name = ENV["AppName"]
-  branch_name = ENV["CODEBUILD_GIT_BRANCH"]
+def set_stack_name(app_name: nil, branch_name: nil)
+  app_name ||= ENV["AppName"]
+  branch_name ||= ENV["CODEBUILD_GIT_BRANCH"]
   stack_name = if branch_name == "master"
     app_name
   else
-    "#{app_name.gsub(/%|_|\//, "-")}-#{branch_name.gsub(/%|_|\//, "-")}"
+    "#{make_name_cfn_friendly(app_name)}-#{make_name_cfn_friendly(branch_name)}"
   end
   stack_name
+end
+
+def make_name_cfn_friendly(name)
+  invalid_characters_regex = /[[:punct:]]/
+  valid_character = "-"
+  invalid_characters = name.scan(invalid_characters_regex).flatten
+  invalid_regex = Regexp.union(invalid_characters.reject { |chr| chr.match(valid_character) })
+  name.gsub(invalid_regex, valid_character)
+end
+
+def delete_stack_on_merge(app_name:, branch_name:, commit_message:)
 end
 
 def run_from_command_line
--- spec/scripts/deploy_spec.rb
+++ spec/scripts/deploy_spec.rb
@@ -2,39 +2,48 @@ require "spec_helper.rb"
 
 require_relative "../../scripts/deploy"
 
-RSpec.describe "set_stack_name" do
-  context "when the branch is not master" do
-    let(:good_stack_name) { "app-name-some-weird-branch-name-2" }
-    before do
-      ENV["CODEBUILD_GIT_BRANCH"] = "some_weird_branch_name_2"
-      ENV["AppName"] = "app-name"
+RSpec.describe "deploy script" do
+  let(:app_name) { "app-name" }
+  describe "#set_stack_name" do
+    context "when the branch is not master" do
+      let(:good_stack_name) { "app-name-some-weird-branch-name-2" }
+      let(:branch_name) { "some_weird_branch_name_2" }
+
+      it "sets the stack name to app name plus branch name" do
+        expect(set_stack_name(app_name: app_name, branch_name: branch_name)).to eq(good_stack_name)
+      end
     end
-    it "sets the stack name to app name plus branch name" do
-      expect(set_stack_name).to eq(good_stack_name)
+
+    context "when the branch name is master" do
+      let(:branch_name) { "master" }
+
+      it "sets the stack name to app name without any suffix" do
+        expect(set_stack_name(app_name: app_name, branch_name: branch_name)).to eq(app_name)
+      end
+    end
+
+    context "when the branch name has non-alpha numeric characters" do
+      let(:good_stack_name) { "app-name-weird-branch-name-3" }
+      let(:branch_name) { "weird/branch*name-3" }
+      let(:app_name) { "app%name" }
+
+      it "replaces any invalid character with a dash" do
+        expect(set_stack_name(app_name: app_name, branch_name: branch_name)).to eq(good_stack_name)
+      end
     end
   end
 
-  context "when the branch name is master" do
-    before do
-      ENV["CODEBUILD_GIT_BRANCH"] = "master"
-      ENV["AppName"] = "app-name"
-    end
-    it "sets the stack name to app name without any suffix" do
-      expect(set_stack_name).to eq("app-name")
-    end
-  end
-  context "it fullfills the cloudformation stack name constraint" do
-    let(:cloudformation_constraint) { /[a-zA-Z][-a-zA-Z0-9]*|arn:[-a-zA-Z0-9:\/._+]*/ }
-
-    let(:good_stack_name) { "special-app-name-weird-branch-name" }
-
-    before do
-      ENV["CODEBUILD_GIT_BRANCH"] = "weird/branch/name"
-      ENV["AppName"] = "special%app_name"
+  describe "#delete_stack_on_merge" do
+    context "when commit message does not indicate a merge commit" do
+      it "does no op" do
+        expect(delete_stack_on_merge(app_name: "some-app", branch_name: "some-branch", commit_message: "some-message")).to be nil
+      end
     end
 
-    it "always makes sure that stack name fullfills" do
-      expect(set_stack_name).to eq(good_stack_name)
+    context "when commit message indicates a merge commit" do
+      it "empties any S3 Buckets"
+
+      it "deletes the stack with the branch name"
     end
   end
 end
