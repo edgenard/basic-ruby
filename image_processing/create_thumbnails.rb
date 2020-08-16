04918c99570162cc46ef96b89c50a89159b03d29
Emmanuel Genard <edgenard@gmail.com>
2020-08-16 12:49:47 -0400

    download file to temp directory from processing bucket; add timeout; add Gemfile for CreateThumbnailFunction

--- image_processing/create_thumbnails.rb
+++ image_processing/create_thumbnails.rb
@@ -1,7 +1,15 @@
+require "aws-sdk-s3"
 module ImageProcessing
   class CreateThumbnail
     def self.handler(event:, context:)
-      p event
+      client = ::Aws::S3::Client.new(region: ENV["AWS_REGION"])
+      uploaded_bucket = event["Records"][0]["s3"]["bucket"]["name"]
+      uploaded_key = event["Records"][0]["s3"]["object"]["key"]
+      client.get_object(response_target: "/tmp/original_file.jpg", bucket: uploaded_bucket, key: uploaded_key)
+      original_file = File.open("/tmp/original_file.jpg")
+    rescue => e
+      p e
+      p e.message
     end
   end
 end
